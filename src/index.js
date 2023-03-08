fae70b71b971cc5c9dd0fc64b2a890b533a58a0a
ViktoriyaSF <kievstar01051985@gmail.com>
2023-03-08 23:38:32 +0200

    task_01

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const url = 'https://restcountries.com/v3.1/name/';
+const restAPI = '?fields=name,capital,population,flags,languages';
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
+
+const inputEL = document.querySelector('input#search-box');
+
+function fetchCountries(name) {
+  return fetch(`${url}${name}${restAPI}`)
+    .then(response => {
+      response.json();
+    })
+    .catch(error => {
+      console.log(Error);
+    });
+}
+
+inputEL.addEventListener('input', debounce(onInputName, DEBOUNCE_DELAY));
